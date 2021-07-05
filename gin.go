e3da4469cda62f9d477cf660b6a1ae5ebe587a7a
Yogi Arif Widodo <yogirenbox33@gmail.com>
2021-07-05 12:33:07 +0800

    yeayyeeyy sudah berhasil

--- gin.go
+++ gin.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"status": "Sudah berhasil yeayeeeyy",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
