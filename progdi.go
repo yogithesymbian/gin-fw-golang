59796769de3409a4811dfeb12ae018e98e4a8b0f
Yogi Arif Widodo <yogirenbox33@gmail.com>
2021-07-05 13:17:30 +0800

    find data

--- progdi.go
+++ progdi.go
@@ -4,9 +4,10 @@ import (
 	/*
 		"bytes"
 		"fmt"
-		"net/http"
 	*/
+
 	"database/sql"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
@@ -21,5 +22,41 @@ func main(){
 	defer db.Close()
 
 	router := gin.Default()
+
+	// modeling
+	type Progdi struct {
+		Id 				int 		`json: "id"`
+		Jenjang 	string 	`json: "jenjang"`
+		NmProgdi 	string 		`json: "nmprogdi"`
+	}
+
+	// show data by ID
+	router.GET("/:id", func(c *gin.Context) {
+		var (
+			progdi Progdi
+			result gin.H
+		)
+
+		id := c.Param("id")
+		row := db.QueryRow("select id, jenjang, nmprogdi from progdi where id = ?;", id)
+		err = row.Scan(&progdi.Id, &progdi.Jenjang, &progdi.NmProgdi)
+
+		if err != nil {
+			// if no result
+			result = gin.H{
+				"status" : "success",
+				"message" : "no data",
+			}
+		} else {
+			result = gin.H{
+				"status" : "success",
+				"message" : "found data",
+				"data" : progdi,
+			}
+		}
+		c.JSON(http.StatusOK, result)
+	})
+
+	// show all Data
 	router.Run(":8080")
 }
